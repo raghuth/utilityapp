8434827d747cbb8dc78402c14aef91028f58873a
raghavendra <raghavendrath21@gmail.com>
2019-05-20 23:35:40 +0530

    Added  registrationBean.ts file

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 
-import { User } from './user/user';
 import { AuthController } from './auth/auth'
 import { MsgController } from './massage/msgcontroller'
+import { Userbean } from './user-bin/user-bin'
 export class App {
 
     constructor() {
         this.msgTest()
         this.authTest()
+        this.userTest()
+        this.beanTest()
     }
 
     msgTest() {
@@ -18,7 +20,26 @@ export class App {
         let auth = new AuthController()
         auth.login('raghu', '1234')
         auth.logout('raghu')
+
     }
+    userTest() {
+        let auth = new AuthController()
+        auth.registration('raghu', 'demo', 9686440121, 'raghavendrath21@gmail.com', 'tumkur,jayanagar west', 'tumkur', '4th main road', 'india')
+    }
+    beanTest() {
+        let auth = new AuthController()
+        let userbean = new Userbean()
+        userbean.username = 'raghu'
+        userbean.password = 'demo'
+        userbean.mobileNumber = 9686440121
+        userbean.emailId = 'raghaendrath21@gmail.com'
+        userbean.address = 'tumkur,jayanagar west'
+        userbean.city = 'tumkur'
+        userbean.street = '4th main road'
+        userbean.country = 'india'
+        auth.registrationBean(userbean)
+    }
+
 
 }
 
--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -1,5 +1,5 @@
 import { User } from '../user/user'
-
+import { Userbean } from '../user-bin/user-bin'
 export class AuthController {
   user: User = new User()
   login(username: string, password: string) {
@@ -10,7 +10,7 @@ export class AuthController {
   logout(username: string) {
     console.log("logout username :", username)
   }
-  registration(username: string, password: number, mobileNumber: number, emailId: string, address: string, city: string, street: string, country: string) {
+  registration(username: string, password: string, mobileNumber: number, emailId: string, address: string, city: string, street: string, country: string) {
     console.log("newUsername :", username)
     console.log("newuserPassword :", password)
     console.log("mobileNumber :", mobileNumber)
@@ -20,7 +20,16 @@ export class AuthController {
     console.log("street :", street)
     console.log("country :", country)
   }
-
+  registrationBean(userbean: Userbean) {
+    console.log("newUsername :", userbean.username)
+    console.log("newuserPassword :", userbean.password)
+    console.log("mobileNumber :", userbean.mobileNumber)
+    console.log("newUseremailid :", userbean.emailId)
+    console.log("address :", userbean.address)
+    console.log("city :", userbean.city)
+    console.log("street :", userbean.street)
+    console.log("country :", userbean.country)
+  }
 }
 
 
